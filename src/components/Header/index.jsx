ff3573f96c607f9543fffbf69e46d5c976cec7a6
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-12-14 18:07:06 -0300

    feat!: create Header and NavBar component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../NavBar"
+
+export const Header = () => {
+    return (
+        <div>
+            <figure>
+                <img src="logo-sampleada.jpg" alt="logo-sampleada" />
+            </figure>
+            <NavBar />
+        </div>
+    )
+}
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">TRACKS</a></li>
+                <li><a href="#">VIDEO</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">EVENTOS</a></li>
+            </ul>
+        </nav>
+    )
+}
